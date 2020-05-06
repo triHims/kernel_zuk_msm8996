4969e8b96bbc9bac9944784645d3a484ac7b4b28
prometheusXderpgang <prometheusXderpgang@gmail.com>
2020-05-06 13:13:52 +0000

    Add a small script for compiling
    Signed-off-by: prometheusXderpgang <prometheusXderpgang@gmail.com>

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 make O=out ARCH=arm64 statix_defconfig
-make -j$(nproc --all) O=out ARCH=arm64 CC=~/clang9 CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=~/gcc9
+make -j$(nproc --all) O=out ARCH=arm64 CC=~/clang9/bin/clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=~/gcc9/bin/aarch64-linux-gnu-
