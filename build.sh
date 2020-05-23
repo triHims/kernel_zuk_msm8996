94b57ee94bb6e1ef3f1ed58b514d448c840c94c0
triHims <himanshu.16bcs1156@abes.ac.in>
2020-05-23 16:39:08 +0000

    move to android4.9 tool chain

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 make O=out ARCH=arm64 statix_defconfig
-make -j$(( $(nproc --all) + 1)) O=out ARCH=arm64 CC=/home/himanshu_16bcs1156/clang9/bin/clang-9 CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=/home/himanshu_16bcs1156/android9/bin/aarch64-linux-gnu-
+make -j$(( $(nproc --all) + 1)) O=out ARCH=arm64 CC=/home/himanshu_16bcs1156/clang9/bin/clang-9 CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=/home/himanshu_16bcs1156/android9/bin/aarch64-linux-android-
