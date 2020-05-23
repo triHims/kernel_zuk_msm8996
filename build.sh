75a1f136800c97c268c24bb6fb505c2c265acb8d
triHims <himanshu.16bcs1156@abes.ac.in>
2020-05-23 13:59:46 +0000

    updated build

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 make O=out ARCH=arm64 statix_defconfig
-make -j$(nproc --all) O=out ARCH=arm64 CC=~/clang9 CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=~/gcc9
+make -j$(( $(nproc --all) + 1)) O=out ARCH=arm64 CC=/home/himanshu_16bcs1156/clang9/bin/clang-9 CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=/home/himanshu_16bcs1156/android9/bin/aarch64-linux-gnu-
